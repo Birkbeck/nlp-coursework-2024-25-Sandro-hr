58069116674d9eb82b368e62daec16056c1fc731
Sandro-hr <sandroherring@gmail.com>
2025-06-19 16:19:38 +0100

    solved part two a iii

--- PartTwo.py
+++ PartTwo.py
@@ -14,3 +14,11 @@ data = data[data["party"] != "Speaker"]
 top_parties = data["party"].value_counts().nlargest(4).index.tolist()
 
 data = data[data["party"].isin(top_parties)]
+
+#a-iii)
+
+data_clean = data[:]
+
+rows_to_drop = data_clean[data_clean['speech_class'] != "Speech"].index
+
+data_clean = data_clean.drop(rows_to_drop)
