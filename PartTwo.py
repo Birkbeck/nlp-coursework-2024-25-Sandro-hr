7a0c64e0766376e6fff86794f255a2526ba6875a
Sandro-hr <sandroherring@gmail.com>
2025-06-20 12:14:47 +0100

    Following the same structure as above, I added the test and train split, will now need to train and evaluate

--- PartTwo.py
+++ PartTwo.py
@@ -105,3 +105,12 @@ def custom_tokenizer(text):
         if token.isalpha() and token.lower() not in string.punctuation:
             clean_tokens.append(token)
     return clean_tokens
+
+vectorizer = TfidfVectorizer(tokenizer=custom_tokenizer, stop_words='english', max_features=3000)
+
+X = vectorizer.fit_transform(data["speech"])
+y = data["party"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=26, stratify=y
+)
